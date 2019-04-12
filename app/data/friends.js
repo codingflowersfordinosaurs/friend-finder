90bf45e2d4e880d300f8007454adda55d7d74415
Plant Lady <codeplantsbylouie@gmail.com>
2019-04-12 18:13:24 -0400

    friends array with objects

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+let friendList = [
+  {
+    name: "Britney Spears",
+    picture:,
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Justin Timberlake",
+    picture:,
+    scores: [4, 3, 2, 1, 5, 5, 3, 1, 4, 2]
+  },
+  {
+    name: "Kelly Clarkson",
+    picture:,
+    scores: [1, 4, 3, 2, 1, 5, 4, 1, 2, 2]
+  },
+  {
+    name: "Justin Guarini",
+    picture:,
+    scores: [3, 2, 1, 4, 5, 3, 1, 5, 5, 3]
+  },
+  {
+    name: "Sonny",
+    picture:,
+    scores: [2, 3, 1, 5, 4, 5, 5, 2, 3, 4]
+  },
+  {
+    name: "Cher",
+    picture:,
+    scores: [2, 3, 2, 5, 3, 4, 1, 1, 4, 3]
+  },
+];
