009e5b901427dcda6f180ffd5cd93d417509ecec
Plant Lady <codeplantsbylouie@gmail.com>
2019-05-01 12:35:26 -0400

    routes and links changed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,77 @@
-let friendList = [
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+var friends = [
   {
     name: "Britney Spears",
-    picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRevEivA8lmZ2ZXZeNyesTx9sVyY-2FxyON-Lt3X9IInUA2vMkp",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    picture:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRevEivA8lmZ2ZXZeNyesTx9sVyY-2FxyON-Lt3X9IInUA2vMkp",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   },
   {
     name: "Justin Timberlake",
-    picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJxpS0o2_NMN0R7JuRkzsODkJreUjrY25g2kETjE0LSFCgi8l",
-    scores: [4, 3, 2, 1, 5, 5, 3, 1, 4, 2]
+    picture:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJxpS0o2_NMN0R7JuRkzsODkJreUjrY25g2kETjE0LSFCgi8l",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   },
   {
     name: "Kelly Clarkson",
-    picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo4CdI19-6h3hrsJn_G6y2iKujS64EXV1g67EOLhyIkg9PtIaS",
-    scores: [1, 4, 3, 2, 1, 5, 4, 1, 2, 2]
+        picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo4CdI19-6h3hrsJn_G6y2iKujS64EXV1g67EOLhyIkg9PtIaS",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   },
   {
     name: "Justin Guarini",
     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTon5zsIxzyTxRjrW1MJJ5vu2tUIKKc5gw3iDYbjej4EMLsjaEY",
-    scores: [3, 2, 1, 4, 5, 3, 1, 5, 5, 3]
-  },
-  {
-    name: "Sonny",
-    picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5bCBOpBbgBM-GgEvB5LJ9_o6TrD4XpEVyuHAJu0oOnDGSdyTcdQ",
-    scores: [2, 3, 1, 5, 4, 5, 5, 2, 3, 4]
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   },
   {
     name: "Cher",
     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiFbJ0cj86we26b6ChAduzuGuMvVoecAqA5MyLwIjwzf-kqHEUPw",
-    scores: [2, 3, 2, 5, 3, 4, 1, 1, 4, 3]
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   },
+  {
+    name: "Sonny",
+    //     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5bCBOpBbgBM-GgEvB5LJ9_o6TrD4XpEVyuHAJu0oOnDGSdyTcdQ",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  }
 ];
 
-module.exports = friendList;
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
+
+// let friendList = [
+//   {
+//     name: "Britney Spears",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRevEivA8lmZ2ZXZeNyesTx9sVyY-2FxyON-Lt3X9IInUA2vMkp",
+//     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+//   },
+//   {
+//     name: "Justin Timberlake",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJxpS0o2_NMN0R7JuRkzsODkJreUjrY25g2kETjE0LSFCgi8l",
+//     scores: [4, 3, 2, 1, 5, 5, 3, 1, 4, 2]
+//   },
+//   {
+//     name: "Kelly Clarkson",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo4CdI19-6h3hrsJn_G6y2iKujS64EXV1g67EOLhyIkg9PtIaS",
+//     scores: [1, 4, 3, 2, 1, 5, 4, 1, 2, 2]
+//   },
+//   {
+//     name: "Justin Guarini",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTon5zsIxzyTxRjrW1MJJ5vu2tUIKKc5gw3iDYbjej4EMLsjaEY",
+//     scores: [3, 2, 1, 4, 5, 3, 1, 5, 5, 3]
+//   },
+//   {
+//     name: "Sonny",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5bCBOpBbgBM-GgEvB5LJ9_o6TrD4XpEVyuHAJu0oOnDGSdyTcdQ",
+//     scores: [2, 3, 1, 5, 4, 5, 5, 2, 3, 4]
+//   },
+//   {
+//     name: "Cher",
+//     picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiFbJ0cj86we26b6ChAduzuGuMvVoecAqA5MyLwIjwzf-kqHEUPw",
+//     scores: [2, 3, 2, 5, 3, 4, 1, 1, 4, 3]
+//   },
+// ];
+
+// module.exports = friendList;
